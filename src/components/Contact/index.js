5cb94a4a2dd1e8f1c8b4fe1916d6524451901240
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-08-20 14:01:14 -0500

    Render+Style: contact link icons

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './index.css';
+import email from '../../images/email.png'
 
 export default function Contact () {
     return (
-        <div>
-            <div>Contact: Linda Helmick</div>
+        <div className='contact-link-wrapper'>
+            {/* <div>Contact: Linda Helmick</div> */}
+            <img className='mail-icon' alt='' src={email} />
+            <img className='mail-icon' alt='' src={email} />
+            <img className='mail-icon' alt='' src={email} />
         </div>
     );
 }
