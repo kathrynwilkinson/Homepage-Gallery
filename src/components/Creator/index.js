244bb5ab606097672695d7e51c4859bddf854c32
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-08-20 14:15:48 -0500

    Add+Style footer

--- src/components/Creator/index.js
+++ src/components/Creator/index.js
@@ -4,8 +4,22 @@ import './index.css';
 export default function Creator () {
     return (
         <div>
-            <div className='creator'>Created by: Kathryn Wilkinson</div>
+            <footer>
+                <div className='footer-wrapper-1'>
+                    <div className='copyright footer-text'>
+                        &copy; K WILKINSON 2021
+                </div>
+                    <div className='term-of-use'>
+                        <a href='' className='footer-text' target='_blank' rel='noreferrer'>TERMS OF USE</a>
+                    </div>
+                    <div class='privacy-policy'>
+                        <a href='' className='footer-text' target='_blank' rel='noreferrer'>PRIVACY POLICY</a>
+                    </div>
+                    <div class='contact'>
+                        <a href='' className='footer-text' target='_blank' rel='noreferrer'>CONTACT</a>
+                    </div>
+                </div>
+            </footer>
         </div>
-
     );
 }
