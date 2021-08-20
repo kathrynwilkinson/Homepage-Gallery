34330af65f3d2d8981c37c1aed3b569d497dd4dc
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-08-19 19:32:12 -0500

    Style: style+position body-text

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css';
 
 export default function Header () {
     return (
         <div>
-            <div className='body'>Body</div>
+            <div className='body'><strong>Linda Helmick</strong> earned her Ph.D in Curriculum and Instruction and Art Education at Indiana University. She is an artist/researcher/teacher who grounds her work in the belief that creative expression is a basic human right and she promotes creativity as a powerful force for change and transformation. Her areas of expertise include critical creative work with homeless and incarcerated populations and building art education/art therapy curricula to better serve those who have experienced trauma.</div>
         </div>
     );
 }
