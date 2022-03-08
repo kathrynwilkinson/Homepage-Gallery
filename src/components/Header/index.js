ab0e6b700c90be64d5aa46f01c1807c556e6edd9
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-03-08 15:39:23 -0600

    Add Navigation component to Header component, style links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
 import './style.css';
+import React from 'react';
+import Navigation from '../../components/Navigation';
+
 
 export default function Header () {
     return (
         <div>
-            <div className='header-img'>Header Image</div>
+            <div className='header-img'>Header background-image
+                <Navigation />
+            </div>
         </div>
     );
 }
