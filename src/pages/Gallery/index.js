9912d0aaea99d78dda75a9813411183b1fe0800e
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-03-08 17:14:14 -0600

    Set up the Gallery page for images+data to be plugged in

--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -5,7 +5,49 @@ export default function Gallery () {
     return (
         <div className='page-body' id='gallery-page'>
 
-			<div>Gallery</div>
+			<div className='page-header'>Gallery</div>
+            <div id='gallery-card-container'>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+                <div className='gallery-card'>
+                    <div className='thumbnail'></div>
+                    <div className='thumb-info'>Title</div>
+                </div>
+            </div>
 
         </div>
     );
